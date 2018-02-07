bf00e25552b53d90dbee4fe86de7cbbcaf96b059
Cristian Peralta Segura <cbperaltasegura@gmail.com>
2018-02-07 09:19:25 -0500

    add callbacks URL

--- authConfig.js
+++ authConfig.js
@@ -1,16 +1,16 @@
 var facebook = {
-  ID : '703238480065028',
-  Secret : '77795a9efd3aceaed76ab7f811402706',
+  ID : 'YOUR_ID',
+  Secret : 'YOUR_SECRET',
   URL : 'http://localhost:3000/auth/facebook/callback'
 }
 var google = {
-  ID:'676760884226-5g9uhj0104fbgh705easvvjnv2o53q1e.apps.googleusercontent.com',
-  Secret:'cs0v7mOhKRmAtjRljEtbvzVc',
+  ID:'YOUR_ID',
+  Secret:'YOUR_SECRET',
   URL: 'http://localhost:3000/auth/google/callback'
 }
 var github = {
-  ID:'9f9ca597ce9cb867de33',
-  Secret:'60880a0aab2c82e8a7301762d9daf63982af6d45',
+  ID:'YOUR_ID',
+  Secret:'YOUR_SECRET',
   URL: 'http://localhost:3000/auth/github/callback'
 }
 module.exports = {
