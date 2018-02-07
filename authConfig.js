b52f31a94621bc359f2002295ea17379a836e1b5
Cristian Peralta <cbperaltasegura@gmail.com>
2018-02-07 09:16:32 -0500

    updating auth

--- authConfig.js
+++ authConfig.js
@@ -1,16 +1,16 @@
 var facebook = {
-  ID : 'YOUR_ID',
-  Secret : 'YOUR_SECRET',
+  ID : '703238480065028',
+  Secret : '77795a9efd3aceaed76ab7f811402706',
   URL : 'http://localhost:3000/auth/facebook/callback'
 }
 var google = {
-  ID:'YOUR_ID',
-  Secret:'YOUR_SECRET',
+  ID:'676760884226-5g9uhj0104fbgh705easvvjnv2o53q1e.apps.googleusercontent.com',
+  Secret:'cs0v7mOhKRmAtjRljEtbvzVc',
   URL: 'http://localhost:3000/auth/google/callback'
 }
 var github = {
-  ID:'YOUR_ID',
-  Secret:'YOUR_SECRET',
+  ID:'9f9ca597ce9cb867de33',
+  Secret:'60880a0aab2c82e8a7301762d9daf63982af6d45',
   URL: 'http://localhost:3000/auth/github/callback'
 }
 module.exports = {
